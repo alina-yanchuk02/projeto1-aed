0e495d6d88d318e629805c65a02dd53528f1ebf6
Luís Rêgo <luis.rego@centroproduto.com>
2019-01-14 21:23:27 +0000

    best_path

--- tsp_dynamic.c
+++ tsp_dynamic.c
@@ -24,11 +24,14 @@ static int best_path[max_n_cities][1 << 18];
 int tsp_dynamic(int n, int dest, int mask)
 {
   if (mask == ((1 << n) - 1)) // última cidade retorna o caminho de volta à origem
-    return cities[dest].distance[0];
+  {
+    best_path[dest][mask] = -1;
+    return best_distance[dest][mask] = cities[dest].distance[0];
+  }
   if (best_distance[dest][mask] != -1) // retornar distância calculada anteriormente
     return best_distance[dest][mask];
 
-  int i, short_distance = 10000, new_distance;
+  int i, short_i = -1, short_distance = 10000, new_distance;
   for (i = 1; i < n; i++)
   {
     if ((mask & (1 << i)) == 0) // evita calcular caminhos com cidades repetidas
@@ -37,10 +40,11 @@ int tsp_dynamic(int n, int dest, int mask)
       if (new_distance < short_distance)
       {
         short_distance = new_distance;
+        short_i = i;
       }
     }
   }
-
+  best_path[dest][mask] = short_i;
   return best_distance[dest][mask] = short_distance;
 }
 
@@ -61,15 +65,23 @@ int main(int argc, char **argv)
   for (n = 3; n <= n_cities; n++)
   {
     for (int i = 0; i < n; i++)
-    {
       for (int j = 0; j < (1 << n); j++)
-      {
         best_distance[i][j] = -1;
-      }
-    }
 
     int best = tsp_dynamic(n, 0, 1);
     printf("First %d cities: %d\n", n, best);
+
+    printf("Best path: ");
+
+    int dest, mask;
+    dest = 0;
+    mask = 1;
+    while (dest >= 0)
+    {
+      printf("%d ", dest);
+      dest = best_path[dest][mask];
+      mask |= 1 << dest;
+    }
     printf("\n");
   }
   double dt2 = elapsed_time();
